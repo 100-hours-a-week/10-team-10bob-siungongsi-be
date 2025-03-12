35fa8aca1b0fdda186a23969a3e04bc40c17765a
Unagi-zoso <Unagi.zoso@gmail.com>
2025-03-12 16:27:05 +0900

    feat: spotless 자동 포매팅 추가

--- src/test/java/org/bob/siungongsi/SiungongsiApplicationTests.java
+++ src/test/java/org/bob/siungongsi/SiungongsiApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SiungongsiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+	@Test
+	void contextLoads() {}
 }
